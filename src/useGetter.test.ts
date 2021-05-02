1ba6cbc5eb09bd3a75648e63d7a474eacc432805
Ryan <rpbiwer@gmail.com>
2021-05-02 17:30:14 -0400

    test useGetter

---
+++ src/useGetter.test.ts
@@ -0,0 +1,27 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import { useState } from "react";
+import { useGetter } from "./useGetter";
+
+test(`useForceUpdate`, () => {
+  const { result } = renderHook(() => {
+    const [value, setValue] = useState<`after` | `before`>(`before`);
+
+    const getValue = useGetter(value);
+
+    return { setValue, getValue };
+  });
+
+  // getValue should (1) remain constant between re-renders, but (2) should aways return the current value
+  const { getValue } = result.current;
+
+  expect(getValue()).toBe(`before`);
+
+  act(() => result.current.setValue(`after`));
+
+  // (1) remain constant between re-renders
+  // eslint-disable-next-line unicorn/consistent-destructuring
+  expect(result.current.getValue).toBe(getValue);
+
+  // (2) should aways return the current value
+  expect(getValue()).toBe(`after`);
+});
