4a7fb701f0a662426dbfecf25d5ebb4dd1850258
Ryan <rpbiwer@gmail.com>
2021-06-20 15:23:33 -0400

    Upgrade jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+  testEnvironment: `jsdom`,
   testPathIgnorePatterns: [`/lib/`],
   transform: {
     "^.+\\.(ts|tsx)$": `ts-jest`,
