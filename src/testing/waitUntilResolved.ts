ce7e8cd4701cb6c2fd71c077f1c08fc01063effa
Ryan <rpbiwer@gmail.com>
2021-06-25 17:12:22 -0400

    waitUntilResolved

---
+++ src/testing/waitUntilResolved.ts
@@ -0,0 +1,11 @@
+import type { WaitFor, WaitOptions } from "@testing-library/react-hooks";
+import type { UseQueryResult } from "react-query";
+import { isQueryResolved } from "../queries/isQueryResolved";
+
+export async function waitUntilResolved<T>(
+  waitFor: WaitFor,
+  result: () => UseQueryResult<T>,
+  options?: WaitOptions,
+) {
+  await waitFor(() => isQueryResolved(result()), options);
+}
