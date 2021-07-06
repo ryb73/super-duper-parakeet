f4fdade749ceb0ec7ee2c0b29d21e47319f3b5f8
Ryan <rpbiwer@gmail.com>
2021-07-06 02:04:40 -0400

    Cleaner Either handling

--- src/server/decodeInput.ts
+++ src/server/decodeInput.ts
@@ -1,5 +1,4 @@
-import { pipe } from "fp-ts/function";
-import { fold } from "fp-ts/lib/Either";
+import { isLeft } from "fp-ts/lib/Either";
 import type { Errors, Type } from "io-ts";
 
 type Options = {
@@ -24,21 +23,17 @@ export function decodeInput<A, O, Req, Res extends MinimalResponse>(
   return <DataWithInput extends InputObject<A>>(
     handler: (req: Req, res: Res, data: DataWithInput) => Promise<void> | void,
   ) => (req: Req, res: Res, data: Omit<DataWithInput, "input">) => {
-    const input = getInput(req);
-    return pipe(
-      T.decode(input),
-      fold(
-        (e) => {
-          if (onError) onError(e);
+    const decoded = T.decode(getInput(req));
 
-          res.status(400);
-          res.end();
-        },
-        (v) => {
-          const newData = { ...data, input: v } as DataWithInput;
-          return handler(req, res, newData);
-        },
-      ),
-    );
+    if (isLeft(decoded)) {
+      if (onError) onError(decoded.left);
+
+      res.status(400);
+      res.end();
+      return undefined;
+    }
+
+    const newData = { ...data, input: decoded.right } as DataWithInput;
+    return handler(req, res, newData);
   };
 }
