efd02f08d8ecaaff14dbfcaab707c9d83cf7dbd1
Ryan <rpbiwer@gmail.com>
2021-07-12 17:51:49 -0400

    querystringize

---
+++ src/queries/querystringize.ts
@@ -0,0 +1,24 @@
+import reduce from "lodash/reduce";
+
+type InValue = boolean | number | string | null | undefined;
+
+type Strategy = (key: string, value: InValue) => Record<string, string>;
+
+// eslint-disable-next-line func-style
+export const defaultStrategy: Strategy = (key, value) => ({
+  [key]: String(value),
+});
+
+export function querystringize<T extends Record<string, InValue>>(
+  record: T,
+  strategy: Strategy = defaultStrategy,
+) {
+  return reduce<T, Record<string, string>>(
+    record,
+    (acc, value, key) => ({
+      ...acc,
+      ...strategy(key, value),
+    }),
+    {},
+  );
+}
---
+++ test/queries/querystringize.test.ts
@@ -0,0 +1,41 @@
+import { querystringize } from "../../src/queries/querystringize";
+
+test(`base`, () => {
+  expect(
+    querystringize({
+      bool: true,
+      nul: null,
+      num: 1,
+      str: `stringinging`,
+      und: undefined,
+    }),
+  ).toStrictEqual({
+    bool: `true`,
+    nul: `null`,
+    num: `1`,
+    str: `stringinging`,
+    und: `undefined`,
+  });
+});
+
+test(`strategy`, () => {
+  expect(
+    querystringize(
+      {
+        num: 1,
+        str: `stringinging`,
+        nul: null,
+        und: undefined,
+      },
+      (key, value) => {
+        if (value === null) return { [`${key}Null`]: `true` };
+        if (value === undefined) return {};
+        return { [key]: String(value) };
+      },
+    ),
+  ).toStrictEqual({
+    num: `1`,
+    str: `stringinging`,
+    nulNull: `true`,
+  });
+});
