1744521f3b84e2113081bdfeaf96b13c163bfa07
Ryan <rpbiwer@gmail.com>
2021-07-13 00:39:30 -0400

    Simplify withDeps api

--- src/testing/withDeps.ts
+++ src/testing/withDeps.ts
@@ -1,62 +1,28 @@
-type Def = {
-  type?: string;
-  name: string;
-};
+import { assert } from "../assert";
 
-type TestDef<T> = Def & {
-  type?: "test";
-  name: string;
-  callback: (value: T) => void;
-};
-
-type DescribeDef<T> = Def & {
-  type: "describe";
-  tests: (DescribeDef<T> | TestDef<T>)[];
-};
-
-type Tests<T> = (DescribeDef<T> | TestDef<T>)[];
-
-function registerTests<T>(
-  tests: Tests<T>,
-  getValue: () => T,
-  timeout: number | undefined,
-) {
-  tests.forEach((def) => {
-    switch (def.type) {
-      case undefined:
-      case `test`:
-        it(def.name, () => def.callback(getValue()), timeout);
-        break;
-      case `describe`:
-        describe(def.name, () => registerTests(def.tests, getValue, timeout));
-        break;
-    }
-  });
-}
-
-type Params<T> = {
-  beforeAll?: () => PromiseLike<T> | T;
-  afterAll?: (value: T) => void;
-  tests: Tests<T>;
+type Params<AllValue> = {
+  beforeAll?: () => AllValue | PromiseLike<AllValue>;
+  afterAll?: (value: AllValue) => void;
+  tests: (values: { all: () => AllValue }) => void;
   timeout?: number;
 };
 
 export function withDeps<T>(params: Params<T>) {
-  const {
-    beforeAll: cbBeforeAll,
-    afterAll: cbAfterAll,
-    tests,
-    timeout,
-  } = params;
+  const { beforeAll: cbBeforeAll, afterAll: cbAfterAll, tests } = params;
 
   let value: T | undefined;
 
-  if (cbBeforeAll)
+  if (cbBeforeAll) {
     beforeAll(async () => {
       value = await cbBeforeAll();
     });
 
+    afterAll(() => {
+      value = undefined;
+    });
+  }
+
   if (cbAfterAll) afterAll(() => cbAfterAll(value!));
 
-  registerTests(tests, () => value!, timeout);
+  tests({ all: () => assert(value) });
 }
--- test/testing/withDeps.test.ts
+++ test/testing/withDeps.test.ts
@@ -1,21 +1,12 @@
 import { withDeps } from "../../src/testing/withDeps";
 
 withDeps({
-  tests: [
-    {
-      name: `value`,
-      callback: (v) => expect(v).toBe(1234),
-    },
-    {
-      type: `describe`,
-      name: `describe`,
-      tests: [
-        {
-          name: `test`,
-          callback: (v) => expect(v).toBe(1234),
-        },
-      ],
-    },
-  ],
+  tests: ({ all }) => {
+    test(`value`, () => expect(all()).toBe(1234));
+
+    describe(`describe`, () => {
+      test(`test`, () => expect(all()).toBe(1234));
+    });
+  },
   beforeAll: () => 1234,
 });
