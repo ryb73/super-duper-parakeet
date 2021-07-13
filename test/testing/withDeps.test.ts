e50ec00ab7c3f7dc194fc47106d94cb170ba2806
Ryan <rpbiwer@gmail.com>
2021-07-13 00:31:01 -0400

    describe test

--- test/testing/withDeps.test.ts
+++ test/testing/withDeps.test.ts
@@ -6,6 +6,16 @@ withDeps({
       name: `value`,
       callback: (v) => expect(v).toBe(1234),
     },
+    {
+      type: `describe`,
+      name: `describe`,
+      tests: [
+        {
+          name: `test`,
+          callback: (v) => expect(v).toBe(1234),
+        },
+      ],
+    },
   ],
   beforeAll: () => 1234,
 });
