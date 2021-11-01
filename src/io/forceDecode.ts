9fb1ca810cab635d1b37da087c130d714163cf0a
Ryan <rpbiwer@gmail.com>
2021-11-01 14:15:12 -0400

    Rename arg0

--- src/io/forceDecode.ts
+++ src/io/forceDecode.ts
@@ -5,23 +5,24 @@ import reporter from "io-ts-reporters";
 
 // It's easy to accidentally pass an unawaited promise as a value, which will always result in failure.
 function forceDecode<A, O, I>(
-  arg0: Decode<I, A> | Type<A, O, I>,
+  typeOrDecoder: Decode<I, A> | Type<A, O, I>,
   value: Promise<any>,
   message?: string,
 ): never;
 
 function forceDecode<A, O, I>(
-  arg0: Decode<I, A> | Type<A, O, I>,
+  typeOrDecoder: Decode<I, A> | Type<A, O, I>,
   value: I,
   message?: string,
 ): A;
 
 function forceDecode<A, O, I>(
-  arg0: Decode<I, A> | Type<A, O, I>,
+  typeOrDecoder: Decode<I, A> | Type<A, O, I>,
   value: I,
   message?: string,
 ): A {
-  const decode = typeof arg0 === `function` ? arg0 : arg0.decode;
+  const decode =
+    typeof typeOrDecoder === `function` ? typeOrDecoder : typeOrDecoder.decode;
 
   return pipe(
     decode(value),
