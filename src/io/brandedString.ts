9c8d7e21305c1f30a68291de20e9ba8343803b54
Ryan <rpbiwer@gmail.com>
2021-11-01 19:33:33 -0400

    brandedNonEmptyString

--- src/io/brandedString.ts
+++ src/io/brandedString.ts
@@ -1,8 +1,19 @@
 import type { Branded } from "io-ts";
 import { brand, string } from "io-ts";
+import { NonEmptyString } from "io-ts-types";
 
 export function brandedString<Brand extends { readonly [K in string]: symbol }>(
   name: string,
 ) {
   return brand(string, (v): v is Branded<string, Brand> => string.is(v), name);
+}
+
+export function brandedNonEmptyString<
+  Brand extends { readonly [K in string]: symbol },
+>(name: string) {
+  return brand(
+    NonEmptyString,
+    (v): v is Branded<NonEmptyString, Brand> => NonEmptyString.is(v),
+    name,
+  );
 }
