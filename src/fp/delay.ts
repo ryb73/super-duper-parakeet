d33fb7abc9202607f4ab3e675b8de0e64ce07d13
Ryan <rpbiwer@gmail.com>
2023-04-11 15:53:27 -0400

    Upgrade eslint-config

--- src/fp/delay.ts
+++ src/fp/delay.ts
@@ -1,5 +1,6 @@
 import { delay as fpDelay } from "fp-ts/lib/Task";
 
 export function delay(ms: number) {
+  // eslint-disable-next-line total-functions/no-premature-fp-ts-effects
   return fpDelay(ms)(() => Promise.resolve())();
 }
