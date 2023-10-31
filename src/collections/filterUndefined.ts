09b92b4654917e2d60a15c474d3c6d4e44187ad4
Ryan <rpbiwer@gmail.com>
2023-10-31 16:56:56 -0400

    Deprecate filterUndefined

--- src/collections/filterUndefined.ts
+++ src/collections/filterUndefined.ts
@@ -1,5 +1,8 @@
 import { isDefined } from "../type-checks";
 
+/**
+ * @deprecated Use `.filter(isDefined)` instead
+ */
 export function filterUndefined<T>(arr: (T | null | undefined)[]): T[] {
   return arr.filter((v) => isDefined(v)) as T[];
 }
